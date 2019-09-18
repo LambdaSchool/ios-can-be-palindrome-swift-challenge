3eea3fc4a6ec50b33309294ef12fc8c72d687342
Isvvc <lyons@tuta.io>
2019-09-18 10:43:18 -0600

    Implement canBePalindrome function

--- canBePalindrome.playground/Contents.swift
+++ canBePalindrome.playground/Contents.swift
@@ -1,7 +1,26 @@
 import Foundation
 
 func canBePalindrome(_ string: String) -> Bool {
-
+    var newString: [Character] = []
+    var isPalindrome: Bool?
+    var i = 0
+    let length = string.count - 1
+    let halfLength = Int(round(Double(string.count) / 2.0))
+    
+    for character in string {
+        newString.append(character)
+    }
+    
+    repeat {
+        if newString[i] != newString[length - i] {
+            isPalindrome = false
+        } else if i >= halfLength - 1 {
+            isPalindrome = true
+        }
+        i += 1
+    } while isPalindrome == nil
+    
+    return isPalindrome ?? false
 }
 
 
