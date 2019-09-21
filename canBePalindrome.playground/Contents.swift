118c134ade6a52cd8ad7621b71a93422cb30da19
Joel Groomer <joelgroomer@gmail.com>
2019-09-21 09:30:50 -0700

    complete

--- canBePalindrome.playground/Contents.swift
+++ canBePalindrome.playground/Contents.swift
@@ -1,7 +1,19 @@
 import Foundation
 
 func canBePalindrome(_ string: String) -> Bool {
-
+    let filteredString = string.filter { (char) -> Bool in
+        switch char {
+        case "a"..."z":
+            return true
+        case "A"..."Z":
+            return true
+        default:
+            return false
+        }
+    }
+    
+    let reversedString = String(filteredString.reversed())
+    return reversedString == filteredString ? true : false
 }
 
 
