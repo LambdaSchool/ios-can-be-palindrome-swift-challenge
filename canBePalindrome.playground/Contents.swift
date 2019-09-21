38772e907fc05151c05151a7e5f25901e70655b3
Aaron Haughton <dreamcast2000@outlook.com>
2019-09-20 22:40:00 -0400

    initial and final commit

--- canBePalindrome.playground/Contents.swift
+++ canBePalindrome.playground/Contents.swift
@@ -1,7 +1,26 @@
 import Foundation
 
 func canBePalindrome(_ string: String) -> Bool {
-
+    var myString = string
+    var count = myString.count
+    let firstLetter = myString.first
+    let lastLetter = myString.last
+    
+    var result = Bool()
+    repeat  {
+        if firstLetter == lastLetter && count != 1 {
+            myString = String(myString.dropFirst(1))
+            myString = String(myString.dropLast(1))
+            count -= 1
+        } else if count == 1 {
+            count -= 1
+            result = true
+        } else if firstLetter != lastLetter {
+            result = false
+            count = 0
+        }
+    } while count != 0
+    return result
 }
 
 
