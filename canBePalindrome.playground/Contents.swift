9172e79de814589117136f30cf3526f6b51fbbe3
Lambda_School_Loaner_220 <lambda_school_loaner_220@Lambda-School-Loaner-220s-MacBook-Pro.local>
2019-11-06 08:50:08 -0800

    Could not figure out a solution for this code challenge

--- canBePalindrome.playground/Contents.swift
+++ canBePalindrome.playground/Contents.swift
@@ -1,8 +1,25 @@
 import Foundation
 
-func canBePalindrome(_ string: String) -> Bool {
+var palindrome1 = "tacocat"
+var palindrome2 = "daily"
+var palindrome3 = "a"
+var palindrome4 = "aa"
+var palindrome5 = "hannah"
+var palindrome6 = "abc"
 
+var palindromeArray = [palindrome1, palindrome2, palindrome3, palindrome4, palindrome5, palindrome6]
+
+func canBePalindrome(_ string: String) -> Bool
+{
+    let index = 0
+    
+    for words in palindromeArray
+    {
+        
+    }
+    return false
 }
+
 
 
 // Test Cases
@@ -12,3 +29,4 @@ canBePalindrome("a")        // should return true
 canBePalindrome("aa")       // should return true
 canBePalindrome("hannah")   // should return true
 canBePalindrome("abc")      // should return false
+
