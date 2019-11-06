b08d6ac385bc0af4865e032ce66cce85fb24b4e0
Zack Larsen <zacklarsen1@gmail.com>
2019-11-06 09:43:09 -0700

    epic fail

--- canBePalindrome.playground/Contents.swift
+++ canBePalindrome.playground/Contents.swift
@@ -1,14 +1,24 @@
 import Foundation
 
 func canBePalindrome(_ string: String) -> Bool {
+    let _string = "palindrome"
+    if string == backString {
 
+
+return true
+    } else {
 }
+canBePalindrome(_ : "tacocat")
+canBePalindrome(_ : "daily")
+
+//// Test Cases
+//canBePalindrome("tacocat")  // should return true
+//canBePalindrome("daily")    // should return false
+//canBePalindrome("a")        // should return true
+//canBePalindrome("aa")       // should return true
+//canBePalindrome("hannah")   // should return true
+//canBePalindrome("abc")      // should return false
 
 
-// Test Cases
-canBePalindrome("tacocat")  // should return true
-canBePalindrome("daily")    // should return false
-canBePalindrome("a")        // should return true
-canBePalindrome("aa")       // should return true
-canBePalindrome("hannah")   // should return true
-canBePalindrome("abc")      // should return false
+
+
