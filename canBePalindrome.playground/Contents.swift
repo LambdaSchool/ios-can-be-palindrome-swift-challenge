8f4d186bc7b6d8679e217a0a77c93c2470792ec4
Tobi Tselios <tobi.tselios@gmail.com>
2019-11-06 16:27:43 +0000

    Complete challenge

--- canBePalindrome.playground/Contents.swift
+++ canBePalindrome.playground/Contents.swift
@@ -1,9 +1,16 @@
 import Foundation
 
 func canBePalindrome(_ string: String) -> Bool {
-
+    let reversedString = String(string.reversed())
+    
+    
+    if reversedString == string {
+        print("\(string) in reverse is \(reversedString)")
+        return true
+    } else {
+        return false
+    }
 }
-
 
 // Test Cases
 canBePalindrome("tacocat")  // should return true
