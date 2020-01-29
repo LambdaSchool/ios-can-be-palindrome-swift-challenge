44bd1f012daee8f5655181ea4cb34d8d62f2069f
Keri Levesque <kerianne166@gmail.com>
2020-01-29 11:18:35 -0500

    finished

--- canBePalindrome.playground/Contents.swift
+++ canBePalindrome.playground/Contents.swift
@@ -1,7 +1,10 @@
 import Foundation
 
 func canBePalindrome(_ string: String) -> Bool {
-
+    if string == String(string.reversed()) {
+        return true
+    }
+    return false
 }
 
 
@@ -12,3 +15,6 @@ canBePalindrome("a")        // should return true
 canBePalindrome("aa")       // should return true
 canBePalindrome("hannah")   // should return true
 canBePalindrome("abc")      // should return false
+
+
+// function needs to check if string is the same backwards then return true if not return false
