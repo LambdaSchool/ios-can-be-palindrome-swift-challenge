97b6d905cb880040bf6abd65785f8bf2104417e6
Nick Nguyen <ptnguyen1901@gmail.com>
2020-01-29 11:12:58 -0500

    Code challenge completed

--- canBePalindrome.playground/Contents.swift
+++ canBePalindrome.playground/Contents.swift
@@ -1,7 +1,14 @@
 import Foundation
 
 func canBePalindrome(_ string: String) -> Bool {
-
+    var chacArray = [Character]()
+     let newString = string.lowercased()
+    for chac in newString {
+        chacArray.append(chac)
+    }
+   
+    return newString.reversed() == chacArray
+   //
 }
 
 
@@ -12,3 +19,12 @@ canBePalindrome("a")        // should return true
 canBePalindrome("aa")       // should return true
 canBePalindrome("hannah")   // should return true
 canBePalindrome("abc")      // should return false
+
+
+canBePalindrome("daily")
+canBePalindrome("abc")
+canBePalindrome("1221")
+canBePalindrome("hannah")
+canBePalindrome("1025")
+canBePalindrome("ABBA")
+
