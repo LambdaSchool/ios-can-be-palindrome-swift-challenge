6e5bf72d53d18445e78737965406d22b0eed7a0a
Christopher DeVito <toolofjesus@protonmail.com>
2020-02-10 22:52:11 -0500

    Completed playground

--- canBePalindrome.playground/Contents.swift
+++ canBePalindrome.playground/Contents.swift
@@ -1,7 +1,22 @@
 import Foundation
 
 func canBePalindrome(_ string: String) -> Bool {
-
+    var countOdds: Int = 0
+    for char in string {
+        var countChars: Int = 0
+        for char2 in string {
+            if char == char2 {
+                countChars += 1
+            }
+        }
+        if (countChars % 2) != 0 {
+            countOdds += 1
+        }
+        if countOdds >= 2 {
+            return false
+        }
+    }
+    return true
 }
 
 
