9c0e75b026bd3638792e0682453557e8d48807d3
Jarren Campos <jarrenmcampos@gmail.com>
2020-02-10 20:04:07 -0800

    Finished code challenge

--- canBePalindrome.playground/Contents.swift
+++ canBePalindrome.playground/Contents.swift
@@ -1,14 +1,27 @@
 import Foundation
 
-func canBePalindrome(_ string: String) -> Bool {
 
+func canBePalindrome(myString: String) -> Bool {
+    var myArray: [String] = []
+    
+    for c in myString{
+        myArray.append("\(c)")
+    }
+    let reversedArray: [String] = Array(myArray.reversed())
+    if reversedArray == myArray{
+        print("Yes this is a palindrome")
+        return true
+    }
+    else {
+        print("Not a palindrome")
+        return false
+    }
 }
 
-
 // Test Cases
-canBePalindrome("tacocat")  // should return true
-canBePalindrome("daily")    // should return false
-canBePalindrome("a")        // should return true
-canBePalindrome("aa")       // should return true
-canBePalindrome("hannah")   // should return true
-canBePalindrome("abc")      // should return false
+canBePalindrome(myString: "tacocat")  // should return true
+canBePalindrome(myString: "daily")    // should return false
+canBePalindrome(myString: "a")        // should return true
+canBePalindrome(myString: "aa")       // should return true
+canBePalindrome(myString: "hannah")   // should return true
+canBePalindrome(myString: "abc")      // should return false
