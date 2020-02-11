7d080a10c676b043fc7d6ac2d9e4f55488fc68fb
Jordan Skenandore <jordan.skenandore@gmail.com>
2020-02-10 20:49:46 -0700

    Some fun palindromes input

--- canBePalindrome.playground/Contents.swift
+++ canBePalindrome.playground/Contents.swift
@@ -21,3 +21,11 @@ canBePalindrome("a")        // should return true
 canBePalindrome("aa")       // should return true
 canBePalindrome("hannah")   // should return true
 canBePalindrome("abc")      // should return false
+canBePalindrome("mom")
+canBePalindrome("racecar")
+canBePalindrome("lionoil")
+canBePalindrome("neveroddoreven")
+canBePalindrome("amanaplanacanalpanama")
+canBePalindrome("Rats live on no evil staR")
+canBePalindrome("Live on time emit no evil")
+canBePalindrome("step on no pets")
