0aa95667545f5cacc8a6fc13b4ff38515a03c651
Bryan Cress <8675309+cressbj@users.noreply.github.com>
2020-05-20 12:03:11 -0400

    got it

--- canBePalindrome.playground/Contents.swift
+++ canBePalindrome.playground/Contents.swift
@@ -1,28 +1,29 @@
 import Foundation
 
 func canBePalindrome(_ string: String) -> Bool {
-    var str = "tacocat"
-    
-    var reverseStr = ""
-    
-    for character in str {
-        reverseStr = character + reverseStr
-        if str == reverseStr {
-         print("\(str) is a Palindrome")
-        } else {
-            print("\(str) is not a Palindrome")
-    }
-}
 
+    let str: String
+    
+    str = "tacocat"
+    let reverseStr = String(str.reversed())
+    
+    
+    if str == reverseStr {
+        return true
+    } else {
+        return false
+}
+}
+    print(canBePalindrome("tacocat"))
 
 // Test Cases
-canBePalindrome("tacocat")  // should return true
-canBePalindrome("daily")    // should return false
-canBePalindrome("a")        // should return true
-canBePalindrome("aa")       // should return true
-canBePalindrome("hannah")   // should return true
-canBePalindrome("abc")      // should return false
+//canBePalindrome("tacocat")  // should return true
+//canBePalindrome("daily")    // should return false
+//canBePalindrome("a")        // should return true
+//canBePalindrome("aa")       // should return true
+//canBePalindrome("hannah")   // should return true
+//canBePalindrome("abc")      // should return false
 
 
 
-}
+
